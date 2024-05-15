909e064103efbe1de2231e30730469d5379fd44f
HChenX <HChen_LXi@outlook.com>
2024-05-15 13:20:32 +0800

    Initial commit

---
+++ app/src/main/java/com/hchen/autoseffswitch/XposedInit.java
@@ -0,0 +1,14 @@
+package com.hchen.autoseffswitch;
+
+import com.hchen.hooktool.HookInit;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedInit implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        HookInit.setTAG("AutoSEffSwitch");
+        HookInit.initLoadPackageParam(lpparam);
+    }
+}
