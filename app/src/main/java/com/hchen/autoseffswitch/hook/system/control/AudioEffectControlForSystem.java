6059df0713038aaad1b7744e4669baf4298de7b5
Voemp <Hoshinakiyoru@163.com>
2024-12-31 17:41:39 +0800

    opt: 切换沉浸声 & 整理非 FW 代码

--- app/src/main/java/com/hchen/autoseffswitch/hook/system/control/AudioEffectControlForSystem.java
+++ app/src/main/java/com/hchen/autoseffswitch/hook/system/control/AudioEffectControlForSystem.java
@@ -35,11 +35,6 @@ import static com.hchen.hooktool.tool.CoreTool.hookMethod;
 import static com.hchen.hooktool.tool.CoreTool.newInstance;
 
 import android.content.Context;
-import android.media.AudioManager;
-import android.os.Handler;
-import android.os.Message;
-
-import androidx.annotation.NonNull;
 
 import com.hchen.autoseffswitch.hook.system.callback.IControlForSystem;
 import com.hchen.hooktool.hook.IHook;
@@ -57,18 +52,12 @@ public class AudioEffectControlForSystem extends BaseEffectControl implements IC
     public static final String TAG = "AudioEffectControlForSystem";
     private static final UUID mDolbyUUID = UUID.fromString("9d4921da-8225-4f29-aefa-39537a04bcaa");
     private static final UUID mMiSoundUUID = UUID.fromString("5b8e36a5-144a-4c38-b1d7-0002a5d5c51b");
-    private static final int STATE_CHANGE_SPATIALIZER = 1;
-    private static final int STATE_CHANGE_MUTED = 2;
-    private final Object mLock = new Object();
-    private Handler mHandler;
-    private boolean mMutedInternal = false;
     private Class<?> mAudioManagerClass = null;
     private Class<?> mMiSoundClass = null;
-    private Class<?> mDoblyClass = null;
-    private Class<?> mIntactDolbyClass = null;
-    private Object mDolbyEffectInstance = null;
+    private Class<?> mDolbyClass = null;
+    private boolean isIntactDolbyClass = false;
+    private Object mDolbyInstance = null;
     private Object mMiSoundInstance = null;
-    private AudioManager mAudioManager;
     private boolean mLastDolbyEnable = false;
     private boolean mLastMiSoundEnable = false;
     private boolean mLastSpatializerEnable = false;
@@ -77,10 +66,11 @@ public class AudioEffectControlForSystem extends BaseEffectControl implements IC
     public void init() {
         mAudioManagerClass = findClass("android.media.AudioManager").get();
         mMiSoundClass = findClass("android.media.audiofx.MiSound").get();
-        if (existsClass("com.dolby.dax.DolbyAudioEffect"))
-            mIntactDolbyClass = findClass("com.dolby.dax.DolbyAudioEffect").get();
-        else
-            mDoblyClass = findClass("com.android.server.audio.dolbyeffect.DolbyEffectController$DolbyAudioEffectHelper").get();
+        if (existsClass("com.dolby.dax.DolbyAudioEffect")) {
+            mDolbyClass = findClass("com.dolby.dax.DolbyAudioEffect").get();
+            isIntactDolbyClass = true;
+        } else
+            mDolbyClass = findClass("com.android.server.audio.dolbyeffect.DolbyEffectController$DolbyAudioEffectHelper").get();
 
         hookMethod("android.media.audiofx.AudioEffect",
                 "setEnabled",
@@ -91,8 +81,8 @@ public class AudioEffectControlForSystem extends BaseEffectControl implements IC
                         observeCall();
                         if (!getEarPhoneStateFinal()) return;
 
-                        if (mDolbyEffectInstance != null) {
-                            if (Objects.equals(mDolbyEffectInstance, thisObject())) {
+                        if (mDolbyInstance != null) {
+                            if (Objects.equals(mDolbyInstance, thisObject())) {
                                 logI(TAG, "earphone is connection, skip set dolby effect!!");
                                 setResult(0); // SUCCESS
                                 return;
@@ -138,147 +128,93 @@ public class AudioEffectControlForSystem extends BaseEffectControl implements IC
         );
     }
 
-    public void initEffect(Context context, AudioManager audioManager) {
-        mAudioManager = audioManager;
-        mDolbyEffectInstance = newInstanceDolbyEffect();
-        mMiSoundInstance = newInstanceMiSound();
 
-        mHandler = new Handler(context.getMainLooper()) {
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                switch (msg.what) {
-                    case STATE_CHANGE_SPATIALIZER -> {
-                        Object sService = callStaticMethod(mAudioManagerClass, "getService");
-                        callMethod(sService, "setSpatializerEnabled", msg.obj);
+    // -------- Effect Utils --------
 
-                        if (mMutedInternal) {
-                            mHandler.sendMessageDelayed(mHandler.obtainMessage(STATE_CHANGE_MUTED), 200L);
-                        }
-                    }
-                    case STATE_CHANGE_MUTED -> {
-                        muteMusicStream(false);
-                    }
-                }
-            }
-        };
-    }
-
-    private boolean isIntactDolbyClass() {
-        return mIntactDolbyClass != null;
-    }
-
-    private void releaseDolbyEffectIfNeed() {
-        if (mDolbyEffectInstance == null)
-            mDolbyEffectInstance = newInstanceDolbyEffect();
-
-        if (mDolbyEffectInstance == null) return;
-        if (hasControlDolby()) return;
-
-        callMethod(mDolbyEffectInstance, "release");
-        mDolbyEffectInstance = null;
-        mDolbyEffectInstance = newInstanceDolbyEffect();
-    }
-
-    private void releaseMiSoundIfNeed() {
-        if (mMiSoundInstance == null)
-            mMiSoundInstance = newInstanceMiSound();
-
-        if (mMiSoundInstance == null) return;
-        if (hasControlMiSound()) return;
-
-        callMethod(mMiSoundInstance, "release");
-        mMiSoundInstance = null;
-        mMiSoundInstance = newInstanceMiSound();
-    }
-
-    private Object newInstanceDolbyEffect() {
-        if (mDoblyClass == null && mIntactDolbyClass == null) return null;
-        if (isIntactDolbyClass())
-            return newInstance(mIntactDolbyClass, 0, 0);
-        else
-            return newInstance(mDoblyClass, 0, 0);
-    }
-
-    private Object newInstanceMiSound() {
-        if (mMiSoundClass == null) return null;
-        return newInstance(mMiSoundClass, 0, 0);
-    }
-
-    private boolean hasControlDolby() {
-        if (mDolbyEffectInstance == null) return false;
-        return (boolean) callMethod(mDolbyEffectInstance, "hasControl");
-    }
-
-    private boolean hasControlMiSound() {
-        if (mMiSoundInstance == null) return false;
-        return (boolean) callMethod(mMiSoundInstance, "hasControl");
-    }
-
-    private void setEnableDolbyEffect(boolean enable) {
-        if (mDolbyEffectInstance == null) return;
-        releaseDolbyEffectIfNeed();
-
-        if (isIntactDolbyClass()) {
-            callMethod(mDolbyEffectInstance, "setBoolParam", 0, enable);
-        } else {
-            byte[] bArr = new byte[12];
-            int int32ToByteArray = 0 + int32ToByteArray(0, bArr, 0);
-            int32ToByteArray(enable ? 1 : 0, bArr, int32ToByteArray + int32ToByteArray(1, bArr, int32ToByteArray));
-            callMethod(mDolbyEffectInstance, "checkReturnValue", callMethod(mDolbyEffectInstance, "setParameter", 5, bArr));
+    private Object initEffectInstance(Object instance, Class<?> cls) {
+        if (cls == null) return null;
+        if (instance != null) {
+            if (hasControl(instance)) return instance;
+            callMethod(instance, "release");
         }
-
-        setEnableEffect(mDolbyEffectInstance, enable);
+        return newInstance(cls, 0, 0);
     }
 
-    private int int32ToByteArray(int src, byte[] dst, int index) {
-        if (isIntactDolbyClass())
-            return (int) callStaticMethod(mIntactDolbyClass, "int32ToByteArray", src, dst, index);
-        else
-            return (int) callStaticMethod(mDoblyClass, "int32ToByteArray", src, dst, index);
-    }
-
-    private void setEnableMiSound(boolean enable) {
-        if (mMiSoundInstance == null) return;
-        releaseMiSoundIfNeed();
-
-        callMethod(mMiSoundInstance, "checkStatus", callMethod(mMiSoundInstance, "setParameter", 25, enable ? 1 : 0));
-        setEnableEffect(mMiSoundInstance, enable);
+    private boolean hasControl(Object instance) {
+        if (instance == null) return false;
+        return (boolean) callMethod(instance, "hasControl");
     }
 
     private void setEnableEffect(Object instance, boolean enable) {
         if (instance == null || instance.getClass().getSuperclass() == null) return;
         Class<?> superEffectClass = instance.getClass().getSuperclass();
         Method native_setEnabled = findMethod(superEffectClass, "native_setEnabled", boolean.class).get();
-        callMethod(mMiSoundInstance, "checkState", "setEnabled()");
+        callMethod(instance, "checkState", "setEnabled()");
         callMethod(instance, native_setEnabled, enable); // super private
     }
 
-    private void setEnable3dSurround(boolean enable) {
+
+    // -------- Dolby --------
+
+    private void setEnableDolbyEffect(boolean enable) {
+        if (mDolbyClass == null) return;
+        mDolbyInstance = initEffectInstance(mDolbyInstance, mDolbyClass);
+
+        if (isIntactDolbyClass) {
+            callMethod(mDolbyInstance, "setBoolParam", 0, enable);
+        } else {
+            byte[] bArr = new byte[12];
+            int int32ToByteArray = int32ToByteArray(0, bArr, 0);
+            int32ToByteArray(enable ? 1 : 0, bArr, int32ToByteArray + int32ToByteArray(1, bArr, int32ToByteArray));
+            callMethod(mDolbyInstance, "checkReturnValue", callMethod(mDolbyInstance, "setParameter", 5, bArr));
+        }
+
+        setEnableEffect(mDolbyInstance, enable);
+    }
+
+    private boolean isEnabledDolbyEffect() {
+        if (mDolbyClass == null) return false;
+        mDolbyInstance = initEffectInstance(mDolbyInstance, mDolbyClass);
+
+        if (isIntactDolbyClass) {
+            return (boolean) callMethod(mDolbyInstance, "getBoolParam", 0);
+        } else {
+            byte[] baValue = new byte[12];
+            int32ToByteArray(0, baValue, 0);
+            callMethod(mDolbyInstance, "checkReturnValue", callMethod(mDolbyInstance, "getParameter", 5, baValue));
+            int en = byteArrayToInt32(baValue);
+            return en > 0;
+        }
+    }
+
+    private int int32ToByteArray(int src, byte[] dst, int index) {
+        return (int) callStaticMethod(mDolbyClass, "int32ToByteArray", src, dst, index);
+    }
+
+    private static int byteArrayToInt32(byte[] ba) {
+        return ((ba[3] & 255) << 24) | ((ba[2] & 255) << 16) | ((ba[1] & 255) << 8) | (ba[0] & 255);
+    }
+
+
+    // -------- MiSound --------
+
+    private void setEnableMiSound(boolean enable) {
         if (mMiSoundInstance == null) return;
-        callMethod(mMiSoundInstance, "checkStatus", callMethod(mMiSoundInstance, "setParameter", 20, enable ? 1 : 0));
+        mMiSoundInstance = initEffectInstance(mMiSoundInstance, mMiSoundClass);
+
+        callMethod(mMiSoundInstance, "checkStatus", callMethod(mMiSoundInstance, "setParameter", 25, enable ? 1 : 0));
+        setEnableEffect(mMiSoundInstance, enable);
     }
 
-    private void setEnableSpatializer(boolean enable) {
-        synchronized (mLock) {
-            if (mAudioManagerClass == null) return;
-            if (!isAvailableSpatializer()) return;
-            boolean alreadyMuted = this.mAudioManager.isStreamMute(3);
-            if (!alreadyMuted || mMutedInternal) {
-                mHandler.removeMessages(STATE_CHANGE_MUTED);
-                muteMusicStream(true);
-            }
-            mHandler.removeMessages(STATE_CHANGE_SPATIALIZER);
-            mHandler.sendMessageDelayed(mHandler.obtainMessage(STATE_CHANGE_SPATIALIZER, enable), 100L);
-        }
+    private boolean isEnabledMiSound() {
+        if (mMiSoundClass == null) return false;
+        mMiSoundInstance = initEffectInstance(mMiSoundInstance, mMiSoundClass);
+
+        return (boolean) callMethod(mMiSoundInstance, "getEnabled");
     }
 
-    private void muteMusicStream(boolean muted) {
-        synchronized (mLock) {
-            mMutedInternal = muted;
-            int direction = muted ? -100 : 100;
-            this.mAudioManager.adjustStreamVolume(3, direction, 0);
-        }
-    }
+
+    // -------- Spatializer --------
 
     private boolean isAvailableSpatializer() {
         if (mAudioManagerClass == null) return false;
@@ -286,30 +222,25 @@ public class AudioEffectControlForSystem extends BaseEffectControl implements IC
         return (boolean) callMethod(sService, "isSpatializerAvailable");
     }
 
-    private boolean isEnabledDolbyEffect() {
-        if (mDolbyEffectInstance == null) return false;
-        releaseDolbyEffectIfNeed();
-
-        if (isIntactDolbyClass()) {
-            return (boolean) callMethod(mDolbyEffectInstance, "getBoolParam", 0);
-        } else {
-            byte[] baValue = new byte[12];
-            int32ToByteArray(0, baValue, 0);
-            callMethod(mDolbyEffectInstance, "checkReturnValue", callMethod(mDolbyEffectInstance, "getParameter", 0 + 5, baValue));
-            int en = byteArrayToInt32(baValue);
-            return en > 0;
-        }
+    private void setEnableSpatializer(boolean enable) {
+        if (mAudioManagerClass == null) return;
+        if (!isAvailableSpatializer()) return;
+        Object sService = callStaticMethod(mAudioManagerClass, "getService");
+        callMethod(sService, "setSpatializerEnabled", enable);
     }
 
-    private static int byteArrayToInt32(byte[] ba) {
-        return ((ba[3] & 255) << 24) | ((ba[2] & 255) << 16) | ((ba[1] & 255) << 8) | (ba[0] & 255);
+    private boolean isEnabledSpatializer() {
+        if (mAudioManagerClass == null) return false;
+        Object sService = callStaticMethod(mAudioManagerClass, "getService");
+        return (boolean) callMethod(sService, "isSpatializerEnabled");
     }
 
-    private boolean isEnabledMiSound() {
-        if (mMiSoundClass == null) return false;
-        releaseMiSoundIfNeed();
 
-        return (boolean) callMethod(mMiSoundInstance, "getEnabled");
+    // -------- 3d Surround --------
+
+    private void setEnable3dSurround(boolean enable) {
+        if (mMiSoundInstance == null) return;
+        callMethod(mMiSoundInstance, "checkStatus", callMethod(mMiSoundInstance, "setParameter", 20, enable ? 1 : 0));
     }
 
     private boolean isEnabled3dSurround() {
@@ -319,17 +250,14 @@ public class AudioEffectControlForSystem extends BaseEffectControl implements IC
         return value[0] == 1;
     }
 
-    private boolean isEnabledSpatializer() {
-        if (mAudioManagerClass == null) return false;
-        Object sService = callStaticMethod(mAudioManagerClass, "getService");
-        return (boolean) callMethod(sService, "isSpatializerEnabled");
-    }
+
+    // -------- Control --------
 
     @Override
     void updateEffectMap() {
         mEffectHasControlMap.clear();
-        mEffectHasControlMap.put(EFFECT_DOLBY_CONTROL, String.valueOf(hasControlDolby()));
-        mEffectHasControlMap.put(EFFECT_MISOUND_CONTROL, String.valueOf(hasControlMiSound()));
+        mEffectHasControlMap.put(EFFECT_DOLBY_CONTROL, String.valueOf(hasControl(mDolbyInstance)));
+        mEffectHasControlMap.put(EFFECT_MISOUND_CONTROL, String.valueOf(hasControl(mMiSoundInstance)));
 
         mEffectEnabledMap.clear();
         mEffectEnabledMap.put(EFFECT_DOLBY, String.valueOf(isEnabledDolbyEffect()));
@@ -359,15 +287,15 @@ public class AudioEffectControlForSystem extends BaseEffectControl implements IC
 
     @Override
     public void resetAudioEffect() {
-        if (mLastDolbyEnable)
-            setEnableDolbyEffect(true);
-        else if (mLastMiSoundEnable) {
+        if (mLastMiSoundEnable) {
+            setEnableDolbyEffect(false);
             setEnableMiSound(true);
-        } else if (mLastSpatializerEnable) {
-            setEnableSpatializer(true);
-        } else if (mLast3dSurroundEnable) {
-            setEnable3dSurround(true);
+            setEnable3dSurround(mLast3dSurroundEnable);
+        } else {
+            setEnableDolbyEffect(true);
+            setEnableMiSound(false);
         }
+        setEnableSpatializer(mLastSpatializerEnable);
     }
 
     @Override
